cb93fd51dcf862ae8d9e51e05666d2f11042dad9
Aleksis Vilums <vilumsar@s.dcsdk12.org>
2021-04-01 17:31:32 -0600

    Add 2 Colleges

--- College_App/src/App.java
+++ College_App/src/App.java
@@ -1,33 +1,78 @@
 import javax.swing.*;
-import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.GroupLayout.ParallelGroup;
+import javax.swing.GroupLayout.SequentialGroup;
+
 import java.awt.EventQueue;
+import java.awt.event.*;
 
 public class App extends JFrame {
-    private JLabel name, major, sort;
-    private JTextField nameTextBox, majorTextBox;
+    private JLabel userName, userMajor, sort, collegeSchool, costPerSem, majorPlacement, population, acceptanceRate, avgGPA, avgSAT;
+    private JLabel[][] colleges = new JLabel[12][7];
+    private JTextField userNameTextBox, userMajorTextBox;
     private JButton addButton;
     private JComboBox<String> sortBox;
-    
     public App() {
         initUI();
     }
     
     private void initUI() {
-        name = new JLabel("Name: ");
-        nameTextBox = new JTextField(10);
-        major = new JLabel("Major: ");
-        majorTextBox = new JTextField(10);
+        userName = new JLabel("Name: ");
+        userNameTextBox = new JTextField(10);
+        userMajor = new JLabel("Major: ");
+        userMajorTextBox = new JTextField(10);
         sort = new JLabel("Sort By: ");
         addButton = new JButton("Add new");
+        addButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String name = JOptionPane.showInputDialog("College Name?");
+                College college = new College(name);
+                if(colleges[0][0].getText().isEmpty()){
+                    colleges[0][0].setText("College Name: " + college.getName());
+                    if (userMajorTextBox.getText().isEmpty()){
+                        String text = JOptionPane.showInputDialog("Major?");
+                        userMajorTextBox.setText(text);
+                    }
+                    colleges[0][1].setText("Cost Per Semeser: $" + college.getCost());
+                    colleges[0][2].setText(userMajorTextBox.getText() + " Placement: " + college.getMajor() + "/50");
+                    colleges[0][3].setText("Population: " + Integer.toString(college.getPopulation()));
+                    colleges[0][4].setText("Accepatance Rate: " + String.valueOf(college.getAcceptanceRate()) + "%");
+                    colleges[0][5].setText("Average GPA: " + String.valueOf(college.getGPA()));
+                    colleges[0][6].setText("Average SAT: " + Integer.toString(college.getSAT()));
+                } else if (colleges[1][0].getText().isEmpty()){
+                    colleges[1][0].setText("College Name: " + college.getName());
+                    colleges[1][1].setText("Cost Per Semeser: $" + college.getCost());
+                    colleges[1][2].setText(userMajorTextBox.getText() + " Placement: " + college.getMajor() + "/50");
+                    colleges[1][3].setText("Population: " + Integer.toString(college.getPopulation()));
+                    colleges[1][4].setText("Accepatance Rate: " + String.valueOf(college.getAcceptanceRate()) + "%");
+                    colleges[1][5].setText("Average GPA: " + String.valueOf(college.getGPA()));
+                    colleges[1][6].setText("Average SAT: " + Integer.toString(college.getSAT()));
+                }
+            }
+        });
         String s[] = {"Cost", "Major", "Population", "SAT Scores", "GPA", "Acceptance Rate"};
         sortBox = new JComboBox<>(s);
-        
-        createLayout(name, nameTextBox, major, majorTextBox, sort, addButton, sortBox);//Method called to place
+        collegeSchool = new JLabel("Name");
+        costPerSem = new JLabel("Cost Per Semester");
+        majorPlacement = new JLabel("Major Placement");
+        population = new JLabel("Population");
+        acceptanceRate = new JLabel("Acceptance Rate");
+        avgGPA = new JLabel("Average GPA");
+        avgSAT = new JLabel("Average SAT");
+        for(int i = 0; i < colleges.length; i++){
+            for (int x = 0; x < colleges[i].length; x++){
+                colleges[i][x] = new JLabel("");
+            }
+        }
+
+        createLayout(userName, userNameTextBox, userMajor, userMajorTextBox, sort, addButton, sortBox, collegeSchool, costPerSem, majorPlacement, population, acceptanceRate, avgGPA, avgSAT, 
+                    colleges[0][0], colleges[0][1], colleges[0][2], colleges[0][3], colleges[0][4], colleges[0][5], colleges[0][6],
+                    colleges[1][0], colleges[1][1], colleges[1][2], colleges[1][3], colleges[1][4], colleges[1][5], colleges[1][6]);//Method called to place
 
         setTitle("College App");
         //      width, height
-        setSize(1080, 1920);
+        setSize(1000, 1000);
         setLocationRelativeTo(null);// Center aline on the screen
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// When close is pressed it closes
     }
@@ -36,34 +81,74 @@ public class App extends JFrame {
         var pane = getContentPane();
         var gl = new GroupLayout(pane);
         pane.setLayout(gl);
-
+        
         gl.setAutoCreateGaps(true);
         gl.setAutoCreateContainerGaps(true); // Creates gaps
-        //GroupLayout
-        gl.setHorizontalGroup(gl.createSequentialGroup()
-            .addGroup(gl.createParallelGroup()
-                .addComponent(args[0])
-                .addComponent(args[2]))
-            .addGroup(gl.createParallelGroup()
-                .addComponent(args[1])
-                .addComponent(args[3]))
-            .addGroup(gl.createParallelGroup()
-                .addComponent(args[4])
-                .addComponent(args[5]))
-            .addGroup(gl.createParallelGroup()
-                .addComponent(args[6])));
 
-        
+        SequentialGroup college1StatsHorizontal = gl.createSequentialGroup();
+        ParallelGroup college1StatsVerticle= gl.createParallelGroup();
+        SequentialGroup college2StatsHorizontal = gl.createSequentialGroup();
+        ParallelGroup college2StatsVerticle = gl.createParallelGroup();
+
+
+        //GroupLayout
+        gl.setHorizontalGroup(gl.createParallelGroup(GroupLayout.Alignment.CENTER)
+            .addGroup(gl.createSequentialGroup()
+                .addComponent(args[0])
+                .addComponent(args[1])
+                .addComponent(args[4])
+                .addComponent(args[6]))
+            .addGroup(gl.createSequentialGroup()
+                .addComponent(args[2])
+                .addComponent(args[3])
+                .addComponent(args[5]))
+            .addGroup(gl.createSequentialGroup()
+                .addComponent(args[7])
+                .addComponent(args[8])
+                .addComponent(args[9])
+                .addComponent(args[10])
+                .addComponent(args[11])
+                .addComponent(args[12])
+                .addComponent(args[13]))
+            .addGroup(college1StatsHorizontal)
+            .addGroup(college2StatsHorizontal));
+
+            for (int i = 14; i <= 20; i++){
+                college1StatsHorizontal.addComponent(args[i]);
+            }
+            
+            for (int i = 21; i <= 27; i++){
+                college2StatsHorizontal.addComponent(args[i]);
+            }
+            
         gl.setVerticalGroup(gl.createSequentialGroup()
             .addGroup(gl.createParallelGroup(Alignment.BASELINE)
                 .addComponent(args[0])
                 .addComponent(args[1])
                 .addComponent(args[4])
                 .addComponent(args[6]))
-                .addGroup(gl.createParallelGroup(Alignment.BASELINE)
+            .addGroup(gl.createParallelGroup(Alignment.BASELINE)
                 .addComponent(args[2])
                 .addComponent(args[3])
-                .addComponent(args[5])));
+                .addComponent(args[5]))
+            .addGroup(gl.createParallelGroup()
+                .addComponent(args[7])
+                .addComponent(args[8])
+                .addComponent(args[9])
+                .addComponent(args[10])
+                .addComponent(args[11])
+                .addComponent(args[12])
+                .addComponent(args[13]))
+            .addGroup(college1StatsVerticle)
+            .addGroup(college2StatsVerticle));
+
+            for (int i = 14; i <= 20; i++){
+                college1StatsVerticle.addComponent(args[i]);
+            }
+
+            for (int i = 21; i <= 27; i++){
+                college2StatsVerticle.addComponent(args[i]);
+            }
 
     }
 
@@ -72,5 +157,9 @@ public class App extends JFrame {
             App calc = new App();
             calc.setVisible(true);
         });
+
+        // for(int x = 0; x >= 10; x++) {
+        //     College[] RaCollege = new College[x];
+        // }
     }
 }
--- College_App/src/College.java
+++ College_App/src/College.java
@@ -1,15 +1,15 @@
 import java.util.Random;
-import java.io.*;
 
 public class College {
-    private int sat, cost, population;
+    private int sat, cost, population, major;
     private double gpa, acceptanceRate;
     private String name;
 
-    //Main constructor
-    College(String name, int cost, int population, int acceptanceRate, Double gpa, int sat) {
+    // Main constructor
+    College(String name, int major, int cost, int population, int acceptanceRate, Double gpa, int sat) {
         this.sat = sat;
         this.cost = cost;
+        this.major = major;
         this.population = population;
         this.gpa = gpa;
         this.acceptanceRate = acceptanceRate;
@@ -21,6 +21,7 @@ public class College {
         Random random = new Random();
         this.sat = random.nextInt(1200) + 400;
         this.cost = random.nextInt(10000) + 5000;
+        this.major = random.nextInt(50);
         this.population = random.nextInt(20000) + 10000;
         this.gpa = (random.nextInt(99) / 100) + (random.nextInt(3) + 2);
         if(this.gpa > 4.50) {
@@ -30,9 +31,24 @@ public class College {
         this.name = getRandomName();
     }
 
+    College(String name) {
+        Random random = new Random();
+        this.sat = random.nextInt(1200) + 400;
+        this.cost = random.nextInt(10000) + 5000;
+        this.major = random.nextInt(50);
+        this.population = random.nextInt(20000) + 10000;
+        this.gpa = (random.nextInt(99) / 100) + (random.nextInt(3) + 2);
+        if(this.gpa > 4.50) {
+            this.gpa = this.gpa - (random.nextDouble() + 0.2);
+        }
+        this.acceptanceRate = random.nextInt(99) + 1;
+        this.name = name;
+    }
+
     // Getters:
     public int getSAT(){return this.sat;}
     public int getCost(){return this.cost;}
+    public int getMajor(){return this.major;}
     public int getPopulation(){return this.population;} 
     public double getGPA(){return this.gpa;}
     public double getAcceptanceRate(){return this.acceptanceRate;}
@@ -41,6 +57,7 @@ public class College {
     // Setters:
     public void setSAT(int newSAT){this.sat = newSAT;}
     public void setCost(int newCost){this.cost = newCost;}
+    public void setMajor(int newMajor){this.major = newMajor;}
     public void setPopulation(int newPopulation){this.population = newPopulation;}
     public void setGPA(double newGPA){this.gpa = newGPA;}
     public void setAcceptanceRate(double newAcceptanceRate){this.acceptanceRate = newAcceptanceRate;}
